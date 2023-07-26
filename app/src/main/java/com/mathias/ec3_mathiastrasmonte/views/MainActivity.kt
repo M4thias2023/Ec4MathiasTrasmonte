2e2582a8076540b31e707428807a4792f0c57c9c
M4thias2023 <mathiastrasmonte2022@gmail.com>
2023-07-26 16:02:26 -0500

    primer commit

--- app/src/main/java/com/mathias/ec3_mathiastrasmonte/views/MainActivity.kt
+++ app/src/main/java/com/mathias/ec3_mathiastrasmonte/views/MainActivity.kt
@@ -14,9 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
         val navHostFragment =supportFragmentManager.findFragmentById(R.id.fcv_clothe) as NavHostFragment
         val navController = navHostFragment.navController
         binding.clotheBnv.setupWithNavController(navController);
